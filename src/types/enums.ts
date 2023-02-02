cc5f15c2a0a5ab529a49abc6682a9d3c81709daf
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 17:52:45 +0300

    feat: add enum ResponceMessage

--- src/types/enums.ts
+++ src/types/enums.ts
@@ -7,3 +7,9 @@ export const enum StatusCode {
   CONFLICT = '409',
   SERVER_ERROR = '500',
 }
+
+export const enum ResponceMessage {
+  USER_ALREADY_EXISTS = 'This login is already in use',
+  USER_DOESNT_EXIST = 'This user is not registered',
+  USER_WRONG_PASSWORD = 'This password is incorrect',
+}
