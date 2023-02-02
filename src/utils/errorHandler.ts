cbf067af98b97b48f1bc55a04df416bf03c5fc24
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 18:38:56 +0300

    refactor: add return to func handleError

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -2,7 +2,7 @@ import { Response } from 'express';
 import { StatusCode } from '../types/enums';
 import CustomError from './customError';
 
-export const handleError = (res: Response, error: unknown) => {
+export const handleError = (res: Response, error: unknown): void => {
   if (error instanceof CustomError) {
     res.status(error.status).send({ message: error.message });
   } else if (error instanceof Error) {
