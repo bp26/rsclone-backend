28f96a2d08a0e4dd9b27d8b23192c61cc2f615d0
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 17:56:19 +0300

    feat: add authRouter

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { DB_URL, PORT } from './config.js';
+import AuthRouter from './routers/authRouter.js';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/auth', AuthRouter);
 
 try {
   mongoose.connect(DB_URL);
