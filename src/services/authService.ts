fecb7300f8d3cd507e592fff06316499d34f7dcb
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 18:33:58 +0300

    feat: add AuthService

---
+++ src/services/authService.ts
@@ -0,0 +1,43 @@
+import jwtHandler from '../jwt/jwtHandler';
+import User from '../models/userModel';
+import CustomError from '../utils/customError';
+import { StatusCode } from '../types/enums';
+import { ResponceMessage } from '../types/enums';
+import { IAuthUser, IUser } from '../types/interfaces';
+
+class AuthService {
+  async register(login: string, password: string): Promise<IUser> {
+    const user = await User.findOne({ login });
+    if (user) {
+      throw new CustomError(
+        StatusCode.CONFLICT,
+        ResponceMessage.USER_ALREADY_EXISTS
+      );
+    }
+    const newUser = await User.create({ login, password });
+    return newUser;
+  }
+
+  async login(login: string, password: string): Promise<IAuthUser> {
+    const user = await User.findOne({ login });
+    if (!user) {
+      throw new CustomError(
+        StatusCode.UNAUTHORIZED,
+        ResponceMessage.USER_DOESNT_EXIST
+      );
+    }
+    if (password !== user.password) {
+      throw new CustomError(
+        StatusCode.UNAUTHORIZED,
+        ResponceMessage.USER_WRONG_PASSWORD
+      );
+    }
+    const token = jwtHandler.generateToken(user);
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default new AuthService();
--- src/types/enums.ts
+++ src/types/enums.ts
@@ -12,4 +12,5 @@ export const enum ResponceMessage {
   USER_ALREADY_EXISTS = 'This login is already in use',
   USER_DOESNT_EXIST = 'This user is not registered',
   USER_WRONG_PASSWORD = 'This password is incorrect',
+  USER_NO_TOKEN = 'No token sent',
 }
