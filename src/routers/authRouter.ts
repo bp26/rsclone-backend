83fded7a973081783720f222ca91f8e0b89c1aea
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 18:38:09 +0300

    feat: update AuthRouter

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import authController from '../controllers/authController';
 
 const authRouter = express.Router();
 
-authRouter.post('/register');
+authRouter.post('/register', authController.register);
 
-authRouter.post('/login');
+authRouter.post('/login', authController.login);
 
 export default authRouter;
