e1b9720ae158c794d2a2a712645197c2e94c0d6b
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 17:53:25 +0300

    feat: add func handleError to utils

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,11 @@
+import { Response } from 'express';
+import { StatusCode } from '../types/enums';
+import CustomError from './customError';
+
+export const handleError = (res: Response, error: unknown) => {
+  if (error instanceof CustomError) {
+    res.status(error.status).send({ message: error.message });
+  } else if (error instanceof Error) {
+    res.status(+StatusCode.SERVER_ERROR).send({ message: error.message });
+  }
+};
