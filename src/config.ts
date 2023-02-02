00cf2943b13210ac0bcb8ae75b5d09f4ed60c87f
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 18:03:20 +0300

    feat: add secret key

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const PORT = 3000;
 export const DB_URL =
   'mongodb://ufkjopthxqwim3inigbd:XJAQtZGsvl3idEBbN9v7@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bkdkhsljobyxyki?replicaSet=rs0';
+export const SECRET_KEY = 'Ab94iu1mPdXZ4WOsXX5x';
