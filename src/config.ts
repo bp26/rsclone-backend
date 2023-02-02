27369ace8b16fda9e026e46872866a9fe462c75a
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-02 17:49:54 +0300

    feat: add app config

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const PORT = 3000;
+export const DB_URL =
+  'mongodb://ufkjopthxqwim3inigbd:XJAQtZGsvl3idEBbN9v7@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bkdkhsljobyxyki?replicaSet=rs0';
