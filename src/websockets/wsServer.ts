debf2a71b0fed7b6170ebbe48bbb0891d8e7ec0a
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-04 22:26:42 +0300

    feat: add websocket server

--- src/websockets/wsServer.ts
+++ src/websockets/wsServer.ts
@@ -1,10 +1,19 @@
 import { WebSocketServer } from 'ws';
+import chatService from '../services/chatService.js';
 import server from '../server.js';
 
 const wsServer = new WebSocketServer({ server });
 
 wsServer.on('connection', (websocket) => {
-  websocket.on('message', (data) => {
-    const message = JSON.parse(data.toString());
+  websocket.on('message', async (data) => {
+    try {
+      const message = JSON.parse(data.toString());
+      await chatService.saveMessage(message);
+      wsServer.clients.forEach((client) => {
+        client.send(JSON.stringify(message));
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 });
