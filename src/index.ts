1ff8991dc7d1775c16e6f9583dcddded027f58d9
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-04 11:54:10 +0300

    feat: add get for default route

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import { DB_URL, PORT } from './config.js';
 import AuthRouter from './routers/authRouter.js';
+import { StatusCode } from './types/enums.js';
 
 export const app = express();
 
@@ -9,10 +10,14 @@ app.use(express.json());
 
 app.use('/auth', AuthRouter);
 
+app.get('/', (req, res) => {
+  res.status(+StatusCode.OK).send('Welcome!');
+});
+
 try {
   mongoose.connect(DB_URL);
   app.listen(PORT, () => {
-    console.log('Сервер запущен');
+    console.log(`Server started on port ${PORT}`);
   });
 } catch (error) {
   console.log(error);
