7a4af4097f5f41150f89652a554ed929919d8c45
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-04 18:57:05 +0300

    feat: update Message model

--- src/models/messageModel.ts
+++ src/models/messageModel.ts
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const Message = new mongoose.Schema({
-  login: { type: String, unique: true, required: true },
+  userId: { type: String, unique: true, required: true },
+  user: { type: String, unique: true, required: true },
   content: { type: String, required: true },
   time: { type: String, required: true },
 });
