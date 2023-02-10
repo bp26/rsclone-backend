2478154aece3b5e8503b556b6a524fc8be6e603e
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-10 19:32:47 +0300

    feat: add cors to project

--- src/config.ts
+++ src/config.ts
@@ -2,3 +2,8 @@ export const PORT = 3000;
 export const DB_URL =
   'mongodb+srv://lokum:TmbqkNOksEHi8A4e@cluster0.wclftqx.mongodb.net/?retryWrites=true&w=majority';
 export const SECRET_KEY = 'Ab94iu1mPdXZ4WOsXX5x';
+export const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 import { createServer } from 'http';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import authRouter from './routers/authRouter.js';
 import chatRouter from './routers/chatRouter.js';
 import userRouter from './routers/userRouter.js';
+import { corsOptions } from './config.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.use('/auth', authRouter);
 app.use('/chat', chatRouter);
