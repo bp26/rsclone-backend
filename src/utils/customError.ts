c1e20a33f431685bf02a3386a99f9c7c7e0acd35
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 15:45:01 +0300

    feat: update CustomError

--- src/utils/customError.ts
+++ src/utils/customError.ts
@@ -1,10 +1,12 @@
-import { StatusCode } from '../types/enums.js';
+import { ErrorType, StatusCode } from '../types/enums.js';
 
 class CustomError extends Error {
   status: number;
-  constructor(status: StatusCode, message: string) {
+  type: ErrorType;
+  constructor(status: StatusCode, type: ErrorType, message: string) {
     super(message);
     this.status = +status;
+    this.type = type;
   }
 }
 
