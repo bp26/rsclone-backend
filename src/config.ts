a625e028083e0f9884a928da0e25d77d3142c85d
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 21:55:32 +0300

    feat: add netlify deploy origin

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,10 @@ export const DB_URL =
   'mongodb+srv://lokum:TmbqkNOksEHi8A4e@cluster0.wclftqx.mongodb.net/?retryWrites=true&w=majority';
 export const SECRET_KEY = 'Ab94iu1mPdXZ4WOsXX5x';
 export const corsOptions = {
-  origin: `http://localhost:8080`,
+  origin: [
+    'http://localhost:8080',
+    'https://deploy-preview-6--bespoke-profiterole-1402f4.netlify.app/',
+  ],
   credentials: true,
   optionSuccessStatus: 200,
 };
