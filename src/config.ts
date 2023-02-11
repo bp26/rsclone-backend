8806798163e7e82b1b4bf01a67c68bab0e318eb2
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 15:14:16 +0300

    feat: update cors options

--- src/config.ts
+++ src/config.ts
@@ -3,5 +3,7 @@ export const DB_URL =
   'mongodb+srv://lokum:TmbqkNOksEHi8A4e@cluster0.wclftqx.mongodb.net/?retryWrites=true&w=majority';
 export const SECRET_KEY = 'Ab94iu1mPdXZ4WOsXX5x';
 export const corsOptions = {
-  origin: '*',
+  origin: `http://localhost:8080`,
+  credentials: true,
+  optionSuccessStatus: 200,
 };
