b001db51c6568aace69dd3045fbeb945438699f0
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-11 16:13:02 +0300

    feat: update setAuthCookies func

--- src/utils/setAuthCookies.ts
+++ src/utils/setAuthCookies.ts
@@ -7,7 +7,17 @@ export const setAuthCookies = (
   id: string,
   token: string
 ): void => {
-  res.cookie(AUTH_ID, id, { httpOnly: true, maxAge: COOKIE_AGE });
-  res.cookie(AUTH_TOKEN, token, { httpOnly: true, maxAge: COOKIE_AGE });
+  res.cookie(AUTH_ID, id, {
+    httpOnly: true,
+    maxAge: COOKIE_AGE,
+    sameSite: 'none',
+    secure: true,
+  });
+  res.cookie(AUTH_TOKEN, token, {
+    httpOnly: true,
+    maxAge: COOKIE_AGE,
+    sameSite: 'none',
+    secure: true,
+  });
   res.sendStatus(+StatusCode.OK);
 };
