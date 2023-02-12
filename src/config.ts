c8c97dae8ceb366ad9e46d63240038ecbdc93235
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-12 11:12:56 +0300

    feat: update cors options

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SECRET_KEY = 'Ab94iu1mPdXZ4WOsXX5x';
 export const corsOptions = {
   origin: [
     'http://localhost:8080',
-    'https://deploy-preview-6--bespoke-profiterole-1402f4.netlify.app:443',
+    'https://deploy-preview-6--bespoke-profiterole-1402f4.netlify.app',
   ],
   credentials: true,
   optionSuccessStatus: 200,
