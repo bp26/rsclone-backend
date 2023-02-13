c117fe4fce421e53712a0bab08bcc347f19b3f22
bp26 <94074742+bp26@users.noreply.github.com>
2023-02-13 21:55:24 +0300

    feat: update cors origin for netlify preview

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,8 @@ export const SECRET_KEY = 'Ab94iu1mPdXZ4WOsXX5x';
 export const corsOptions = {
   origin: [
     'http://localhost:8080',
-    'https://deploy-preview-6--bespoke-profiterole-1402f4.netlify.app',
+    'https://deploy-preview-7--bespoke-profiterole-1402f4.netlify.app',
+    'https://deploy-preview-8--bespoke-profiterole-1402f4.netlify.app',
   ],
   credentials: true,
   optionSuccessStatus: 200,
